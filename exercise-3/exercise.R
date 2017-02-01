b5312b0f4cde6e826f31d5c8649c8e99252d0d0c
Megan Truong <mtruong3@uw.edu>
2017-02-01 14:19:01 -0800

    Add code to exercise 3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -9,21 +9,30 @@ library(dplyr)
 # Make a variable base.url that has the same base url from the poke api documentation.
 # (Hint: visit http://pokeapi.co/ to find the base url)
 
+base.url <- "http://pokeapi.co/api/v2/"
+
 
 # Make a variable called movie that has the names of your favorite pokemon
 # be aware of casing! 
 # There are multiple ways to call the pokemon. What are the 2 ways you can call 
 # it with the Poke API? 
 
+movie <- "snorlax"
+
 
 # Make a variable called pokemon url that holds your favorite pokemon's url 
+
+pokemon.url <- paste(base.url, "pokemon/", movie, "/", sep = "")
 
 
 # Use the GET function to call the url, store it in a variable called response
 
+response <- GET(pokemon.url)
 
 # Use the names function to learn more response. Print it out. It will return a list of ten 
 # names within the response object. You can think of the list names as column names. 
+
+print(names(response))
 
 
 # Within the printed list the most important values we are looking at is 
@@ -31,35 +40,35 @@ library(dplyr)
 # Be sure that the code is working by checking it with this page. 
 # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 # hint: remeber you can call this similar to how you would call a column!
-
+print(response$status_code)
 
 # Now that you have confirmed your URL is working, let's read in the JSON file
 # create a variable body that stores the data from the URL
 # what function did we use to extract the data from response?
-
+body <- content(response, "text")
 
 # create a variable poke.data that will convert the JSON string into a list
-
+poke.data <- fromJSON(body)
 
 # Print out the keys from the poke.data by using the names function
-
+names(poke.data)
 
 # Check if poke.data is a data frame. Print that value.
-
+print(is.data.frame(poke.data))
 
 # Check if moves in poke.data is a data frame. Moves is one of the many keys stored in this
 # JSON object. How would you call moves? Print that value.
-
+print(is.data.frame("moves"))
 
 # Food for thought
 # What do you think is the diffrence between the two outputs above? Why are they diffrent?
 
 # Create a data frame named poke.moves with the moves. be sure to use the head function to only get the
 # first few values. Feel free to use view to look at the dataframe.
-
+poke.moves <- data.frame(poke.data$moves) %>% head(3)
 
 # Flatten the poke.moves data frame
-
+poke.moves <- flatten(poke.moves)
 
 # Use the colnames function to learn what columns are in your poke.moves data frame. Print this.
 
